7c62998f5f29fca33658db5fa513aa23fbbd4116
Abdullah Al Sayad <108291857+Sayadkhan@users.noreply.github.com>
2023-05-12 23:29:06 +0600

    navbar design complet

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import Link from "next/link";
+import React from "react";
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <div className="h-20 bg-black text-gray-400 flex items-center">
+      <div className="wrapper flex justify-between">
+        <Link href="/" className="text-white font-semibold">
+          Cordemy
+        </Link>
+        <div className="flex gap-5">
+          <Link href="/" className="hover:text-white transition-colors">
+            Home
+          </Link>
+          <Link href="/courses" className="hover:text-white transition-colors">
+            Courses
+          </Link>
+          <Link href="/about" className="hover:text-white transition-colors">
+            About
+          </Link>
+          <Link href="/contact" className="hover:text-white transition-colors">
+            Contact
+          </Link>
+        </div>
 
-export default Navbar
+        <div>
+          <Link href="/login" className="hover:text-white transition-colors">
+            Sign in
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
--- pages/courses/index.js
+++ pages/courses/index.js
@@ -1,7 +1,20 @@
 import React from "react";
 
 const CoursesPage = () => {
-  return <div>CoursesPage</div>;
+  return (
+    <div className="wrapper py-10">
+      <div className="flex items-center flex-col text-center gap-1">
+        <span className="uppercase text-sm font-bold tracking-widest">
+          Courses
+        </span>
+        <h2 className="text-2xl">Browse all courses</h2>
+        <p className="w-3/5 text-gray-500">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nam eveniet
+          accusamus velit saepe modi suscipit.
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default CoursesPage;
