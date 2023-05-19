379cef0b084236c02bf9a144c4f81e3f390736ca
Abdullah Al Sayad <abdullahalsayad03@gmail.com>
2023-05-19 23:42:57 +0600

    login added

--- pages/users/login.jsx
+++ pages/users/login.jsx
@@ -1,13 +1,48 @@
-
 import SectionHeader from "@/components/SectionHeader";
-import {FcGoogle} from "react-icons/fc"
-import React from "react";
+import { FcGoogle } from "react-icons/fc";
 
-const LoginPage = () => {
-  return <div className="wrapper py-10 min-h-screen">
-    <SectionHeader span={"login"} h2={"get started with google"} p={"plse login"}/>
-    <div><button className="flex gap-2 items-center bg-black text-white py-3 px-6 rounded-lg mt-5"><span><FcGoogle/></span>Sign in with Google</button></div>
-  </div>;
+import { signIn, getSession } from "next-auth/react";
+import { useRouter } from "next/router";
+
+const LoginPage = ({ session }) => {
+  const loginWithGoogle = async () => {
+    try {
+      await signIn("google");
+    } catch (err) {
+      console.log(err.message);
+    }
+  };
+
+  return (
+    <div className="wrapper py-10 min-h-screen">
+      <SectionHeader
+        span={"login"}
+        h2={"get started with google"}
+        p={"plse login"}
+      />
+      <div className="flex justify-center">
+        <button
+          className="flex gap-2 items-center bg-black text-white py-3 px-6 rounded-lg mt-5 hover:bg-gray-700 duration-300"
+          onClick={loginWithGoogle}
+        >
+          <span>
+            <FcGoogle />
+          </span>
+          Sign in with Google
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default LoginPage;
+
+export const getServerSideProps = async (context) => {
+  const session = await getSession(context);
+
+  return {
+    props: {
+      session,
+    },
+  };
+};
