dc7193599d85f91a19d0b9c02fafbef6b51d6590
Abdullah Al Sayad <abdullahalsayad03@gmail.com>
2023-05-19 23:22:27 +0600

    login page design

--- pages/_app.js
+++ pages/_app.js
@@ -2,12 +2,16 @@ import Footer from "@/components/Footer";
 import Navbar from "@/components/Navbar";
 import "@/styles/globals.css";
 
-export default function App({ Component, pageProps }) {
+import {SessionProvider} from "next-auth/react"
+
+export default function App({ Component, pageProps, session }) {
   return (
     <>
-      <Navbar />
+     <SessionProvider session={session}>
+     <Navbar />
       <Component {...pageProps} />
       <Footer />
+     </SessionProvider>
     </>
   );
 }
--- pages/users/login.jsx
+++ pages/users/login.jsx
@@ -1,7 +1,13 @@
+
+import SectionHeader from "@/components/SectionHeader";
+import {FcGoogle} from "react-icons/fc"
 import React from "react";
 
 const LoginPage = () => {
-  return <div>LoginPage</div>;
+  return <div className="wrapper py-10 min-h-screen">
+    <SectionHeader span={"login"} h2={"get started with google"} p={"plse login"}/>
+    <div><button className="flex gap-2 items-center bg-black text-white py-3 px-6 rounded-lg mt-5"><span><FcGoogle/></span>Sign in with Google</button></div>
+  </div>;
 };
 
 export default LoginPage;
