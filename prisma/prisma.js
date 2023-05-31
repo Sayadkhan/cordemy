cabe6f60e7c4fbb9b561ea2f09473b9fd7cf47a5
Abdullah Al Sayad <abdullahalsayad03@gmail.com>
2023-06-01 00:25:09 +0600

    prisma fixed

--- prisma/prisma.js
+++ prisma/prisma.js
@@ -1,18 +1,3 @@
-// import { PrismaClient } from "@prisma/client";
-
-// let prisma;
-
-// if (process.env.NODE_ENV === "production") {
-//   prisma = new PrismaClient();
-// } else {
-//   if (!global.prisma) {
-//     global.prisma = new PrismaClient();
-//   }
-//   prisma = global.prisma;
-// }
-
-// export default prisma;
-
 import { PrismaClient } from "@prisma/client";
 
 let prisma;
